268893d77f28327b2424a033fe324a17b08c06f2
vdiazpa <vdiazpa@sandia.gov>
2025-06-12 16:30:22 -0600

    Began  subproblem Definition

--- egret/models/custom_uc.py
+++ egret/models/custom_uc.py
@@ -1,91 +1,84 @@
-#def build_custom_uc_model(data):
-#    uc_form = ucgen.UCFormulation(
-#        reserve_vars = 'CA_power_avail_vars',
-#        ramping_limits = 'CA_ramping_limits',
-#        production_costs = 'basic_production_costs_envelope',
-#       uptime_downtime = 'DEKT_UT_DT',
-#        startup_costs = 'CA_startup_costs',
-#       network_constraints = 'ptdf_power_flow')
-
 from pyomo.environ import *
 from egret.data.data_utils import _read_from_file
 
-file_path = "/egret/examples/unit_commitment/tiny_uc_tc_solution.json"
+# enter subhorizon 
+subh_n = 2
+#file_path = "examples/unit_commitment/tiny_uc_tc_solution.json"
+file_path = "examples/unit_commitment/tiny_example.json"
 
 model_data_dict = _read_from_file(file_path, file_type="json")
-system = model_data_dict['system']
-elements = model_data_dict['elements']
 
-# Replace with your actual JSON filename
-model_data_dict = _read_from_file(file_path, file_type="json")
-
-system = model_data_dict.get("system", {})
-elements = model_data_dict.get("elements", {})
-
+system      = model_data_dict.get("system", {})
+elements    = model_data_dict.get("elements", {})
 num_periods = len(system['time_keys'])
 
-m = ConcreteModel()
+p_maxx = {}
+p_t0   = {}
 
-# Define Model Sets
-m.TimePeriods        = RangeSet(1, num_periods)
-m.ThermalGenerators  = Set(initialize=elements['generator'].keys())
-m.TransmissionLines  = Set(initialize=elements['branch'].keys())
-m.Buses              = Set(initialize=elements['bus'].keys())
+for key in elements['generator'].keys():
+    p_maxx[key] = elements['generator'][key]['p_max']
+    p_t0[key]   = elements['generator'][key]['initial_p_output']
 
-#Create dicts for Model Parameters
-thermal_limit_dict = { br: br_data['rating_long_term'] for br, br_data in elements['branch'].items()}
 
-def time_mapper(values):
-    return {t+1: val for t, val in enumerate(values)}
+def build_model(s_e):
 
-pmax_dict = {}
-for g_name, g_data in elements['generator'].items():
-    pmax = g_data.get('p_max', None)
-    if isinstance(pmax, dict) and pmax.get('data_type') == 'time_series':
-        for t, val in time_mapper(pmax['values']).items():
-            pmax_dict[(g_name, t)] = val
-    elif isinstance(pmax, (int, float)):
-        for t in range(1, num_periods + 1):
-            pmax_dict[(g_name, t)] = pmax
+    m = ConcreteModel()
 
-p0_dict = { gen: gen_data['initial_p_output'] for gen, gen_data in elements['generator'].items()}
+    # Define Model Sets
+    m.TimePeriods        = s_e
+    m.ThermalGenerators  = Set(initialize=elements['generator'].keys())
+    m.TransmissionLines  = Set(initialize=elements['branch'].keys())
+    m.Buses              = Set(initialize=elements['bus'].keys())
 
-# Define Model Parameters
-m.PowerGeneratedT0   = Param(m.ThermalGenerators, initialize=p0_dict)
-m.ThermalLimit       = Param(m.TransmissionLines, initialize=thermal_limit_dict)
-m.MaximumPowerOutput = Param(m.ThermalGenerators, m.TimePeriods, initialize=pmax_dict)
+    # Define Model Parameters
+    m.PowerGeneratedT0   = Param(m.ThermalGenerators, initialize=p_t0)
+    m.MaximumPowerOutput = Param(m.ThermalGenerators, initialize=p_maxx)
 
-#### Define Variables & Bounds
-#1 - Power
-def power_bounds_rule(m, g, t):
-    return (0, m.MaximumPowerOutput[g,t])
-m.PowerGenerated        = Var(m.ThermalGenerators, m.TimePeriods, within=NonNegativeReals, bounds=power_bounds_rule) 
-m.MaximumPowerAvailable = Var(m.ThermalGenerators, m.TimePeriods, within=NonNegativeReals)
+    #### Define Variables & Bounds
+    #1 - Power
+    def power_bounds_rule(m, g, t):
+        return (0, m.MaximumPowerOutput[g])
+    m.PowerGenerated        = Var(m.ThermalGenerators, m.TimePeriods, within=NonNegativeReals, bounds=power_bounds_rule) 
+    #m.MaximumPowerAvailable = Var(m.ThermalGenerators, m.TimePeriods, within=NonNegativeReals)
 
-#2 - Binaries
-m.UnitOn    = Var(m.ThermalGenerators, m.TimePeriods, within=Binary)
-m.UnitStart = Var(m.ThermalGenerators, m.TimePeriods, within=Binary)
-m.UnitStop  = Var(m.ThermalGenerators, m.TimePeriods, within=Binary)
+    #2 - Binaries
+    m.UnitOn    = Var(m.ThermalGenerators, m.TimePeriods, within=Binary)
+    m.UnitStart = Var(m.ThermalGenerators, m.TimePeriods, within=Binary)
+    m.UnitStop  = Var(m.ThermalGenerators, m.TimePeriods, within=Binary)
 
-# 3 - LineFlow
-def line_bounds_rule(m, l, t):
-    return (-m.ThermalLimit[l], m.ThermalLimit[l])
-m.LinePower = Var(m.TransmissionLines, m.TimePeriods, bounds=line_bounds_rule)
+    #### Model Constraints
+    m.coupling_constraints = ConstraintList()
+    m.logical_constraints  = ConstraintList()
 
-# 4 - Voltage Angles
-m.Theta = Var(m.Buses ,m.TimePeriods, bounds = (-180,180)) #radians or degrees? 
+    def enforce_max_capacity_rule(m, g, t):
+        return m.PowerGenerated[g,t] <= m.MaximumPowerOutput[g]*m.UnitOn[g,t]
+    m.EnforceMaxCapacity = Constraint(m.ThermalGenerators, m.TimePeriods, rule=enforce_max_capacity_rule)
 
-#### Model Constraints
+    for g in m.ThermalGenerators:
+        for t in m.TimePeriods: 
+            if t in m.TimePeriods and t-1 in m.TimePeriods: 
+                m.logical_constraints.add(m.UnitStart[g,t] - m.UnitStop[g,t] == m.UnitOn[g,t] - m.UnitOn[g,t-1])
+            else:
+                m.coupling_constraints.add(m.UnitStart[g,t] - m.UnitStop[g,t] == m.UnitOn[g,t] - m.UnitOn[g,t])    
 
-ref_bus = system['reference_bus']
-def fix_reference_bus_rule(m, t):
-    return m.Angle[ref_bus, t] == system['reference_bus_angle']
-m.ReferenceBusAngle = Constraint(m.TimePeriods, rule=fix_reference_bus_rule)
+    m.pprint()
 
-def enforce_max_capacity_rule(m, g, t):
-    return m.MaximumPowerAvailable[g,t] <= m.MaximumPowerOutput[g,t]*m.UnitOn[g,t]
-m.EnforceMaxCapacity = Constraint(m.ThermalGenerators, m.TimePeriods, rule=enforce_max_capacity_rule)
+    return m
 
+subproblems = []
+for t in range(1,num_periods,subh_n):
+    t_j = [i for i in range(t,t+subh_n)]
+    model = build_model(t_j)
+    subproblems.append(model)
+
+
+
+#m.ThermalLimit       = Param(m.TransmissionLines, initialize=thermal_limit_dict)
+
+# # 3 - LineFlow
+# def line_bounds_rule(m, l, t):
+#    return (-m.ThermalLimit[l], m.ThermalLimit[l])
+# m.LinePower = Var(m.TransmissionLines, m.TimePeriods, bounds=line_bounds_rule)
 # def enforce_max_available_ramp_up_rates_rule(m, g, t):
 #        # 4 cases, split by (t-1, t) unit status (RHS is defined as the delta from m.PowerGenerated[g, t-1])
 #        # (0, 0) - unit staying off:   RHS = maximum generator output (degenerate upper bound due to unit being off)
